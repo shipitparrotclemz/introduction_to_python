274ce62f48117ccd56bfa17285eab8c0277eb10c
ShipItParrot <shipitparrot@gmail.com>
2024-01-07 15:40:59 +0800

    🐛 Fix bug

--- additional_practice_20231210/004_lessons_on_bool/008_combining_logical_operators_bool.py
+++ additional_practice_20231210/004_lessons_on_bool/008_combining_logical_operators_bool.py
@@ -7,5 +7,6 @@
 bool1: bool = True
 bool2: bool = False
 bool3: bool = True
-result: bool = bool1 is True or bool2 is True or bool3 is False
+# result: bool = bool1 or bool2 and bool3 is False
+result: bool = bool1 is True or bool2 is True and bool3 is False
 print(result)
