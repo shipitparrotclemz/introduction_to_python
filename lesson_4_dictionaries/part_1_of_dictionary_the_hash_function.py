df4ad3bbee731d1428e8498de3b325f479635511
ShipItParrot <shipitparrot@gmail.com>
2024-03-10 14:03:26 +0800

    Challenge 1: Create a function that converts a search term into a list index

--- lesson_4_dictionaries/part_1_of_dictionary_the_hash_function.py
+++ lesson_4_dictionaries/part_1_of_dictionary_the_hash_function.py
@@ -1,7 +1,3 @@
-length_of_list: int = 1024
-my_list: list[str] = [None] * length_of_list
-wei_xuan: str = "Wei Xuan"
-
 """
 Challenge 1:
 
@@ -26,5 +22,8 @@ The number must be 0 <= X < length_of_list - 1
 - % as a hack, to convert an integer within a range of 0 to length_of_list
 number % length_of_list -> 0 to length_of_list - 1
 """
+length_of_list: int = 1024
+my_list: list[str] = [None] * length_of_list
+wei_xuan: str = "Wei Xuan"
 weixuan_index: int = hash(wei_xuan) % length_of_list
 print(weixuan_index)
