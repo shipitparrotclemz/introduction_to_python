349faf7b43b17d0b29412e5622b07f038df36d18
ShipItParrot <shipitparrot@gmail.com>
2024-07-28 15:11:06 +0800

    Fix answer key for exercise 8

--- 20240728_booleans_and_if_else/008_combining_logical_operators_bool.py
+++ 20240728_booleans_and_if_else/008_combining_logical_operators_bool.py
@@ -1,27 +1,28 @@
 """
-## Challenge 8: Combining Logical Operators
-- Define three boolean objects True, False, and True and
+Challenge 8: Combining Logical Operators
+
+Define three boolean objects True, False, and True and
 assign their values to variables named bool1, bool2, and bool3 respectively
-- Define a boolean object that checks if either bool1 or bool2 is True, and bool3 is False using the or, and operators and assign it to a variable named result
-- Print result
+
+Define a boolean object that checks if either bool1 or bool2 is True,
+and bool3 is False using the or, and operators and assign it to a variable
+
+named result
+Print result
+python3 008_combining_logical_operators_bool.py
+True
 """
+
 bool1: bool = True
 bool2: bool = False
 bool3: bool = True
-# result: bool = bool1 or bool2 and bool3 is False
-result: bool = bool1 is True or bool2 is True and bool3 is False
-"""
-Q: Is this a valid alternative answer?
-bool1 is True or bool2 is True or bool3 is True
-                                ^^^^^^^^^^^^^^^
 
-a: Lets check it out by creating our own test cases
-and checking if the above answer passes the test case
+# new concept: use or, to return True, if left or right hand side is True
 
-Input, Expected, Output
-TC1: False, True, False -> True => True (Passed)
-TC2: False, False, True -> False => True (Failed)
-TC3: False, True, False -> True
-TC4: False, False, False -> False
-"""
+# bool1 or bool2 -> True or False -> or will return True
+# True and bool3 is False
+# True and False
+# False
+result: bool = (bool1 or bool2) and not bool3
+
 print(result)
