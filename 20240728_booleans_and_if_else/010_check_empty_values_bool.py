cdb7bf32175a80e1e90bfb0726e22b439cec9eb5
ShipItParrot <shipitparrot@gmail.com>
2024-07-28 15:22:07 +0800

    Fix answer key for exercise 10

--- 20240728_booleans_and_if_else/010_check_empty_values_bool.py
+++ 20240728_booleans_and_if_else/010_check_empty_values_bool.py
@@ -1,10 +1,24 @@
 """
-## Challenge 10: Checking for Empty Values
-- Define an empty list [] and assign it to a variable named empty_list
-- Define a boolean object that checks if empty_list is empty using the not operator and assign it to a variable named is_empty
-- Print is_empty
+Challenge 10: Checking for Empty Values
+Define an empty list [] and assign it to a variable named empty_list
+
+Define a boolean object that checks if empty_list is empty using the
+not operator and assign it to a variable named is_empty
+
+Print is_empty
+python3 010_check_empty_values_bool.py
+True
 """
-my_milk: int = 0
-if 10 % 2 == 0:  # True
-    my_milk += 1
-print(my_milk)
+
+# New Concept:
+# In python, we have this new data structure called a list
+# a list is like an egg tray
+# Creating a list:
+# <variable_name>: list[<type_the_list_contains>] = []
+
+empty_list: list[str] = []
+# New Concept: if you put the `not` keyword in front of a data structure like list
+# it will return True, if the data structure is empty
+is_empty: bool = not empty_list     # False
+# False
+print(is_empty)
