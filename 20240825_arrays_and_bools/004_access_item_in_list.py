f965151205f86bbe7bbbf42d429de15c8607ca44
ShipItParrot <shipitparrot@gmail.com>
2024-08-25 12:29:28 +0800

    Add access item in list

---
+++ 20240825_arrays_and_bools/004_access_item_in_list.py
@@ -0,0 +1,19 @@
+"""
+Create a list containing 7 names
+
+my_list: list[str] = ["Gabriel", "Aaron", "Clement", "Xuan", "Vivienne", "Elson", "Eugene"]
+
+Get Elson from list, print it
+
+Hint: to get index of an item in a list
+
+my_list.index("Elson")
+
+Hint: index of items start from zero
+
+Hint: to get item from list by index
+
+To get Gabriel
+
+my_list[0]
+"""
---
+++ 20240825_arrays_and_bools/solutions/004_access_item_in_list.py
@@ -0,0 +1,23 @@
+"""
+Create a list containing 7 names
+
+my_list: list[str] = ["Gabriel", "Aaron", "Clement", "Xuan", "Vivienne", "Elson", "Eugene"]
+
+Get Elson from list, print it
+
+Hint: to get index of an item in a list
+
+my_list.index("Elson")
+
+Hint: index of items start from zero
+
+Hint: to get item from list by index
+
+To get Gabriel
+
+my_list[0]
+"""
+
+my_list: list[str] = ["Gabriel", "Aaron", "Clement", "Xuan", "Vivienne", "Elson", "Eugene"]
+elson_index: int = my_list.index("Elson")
+print(my_list[elson_index])
