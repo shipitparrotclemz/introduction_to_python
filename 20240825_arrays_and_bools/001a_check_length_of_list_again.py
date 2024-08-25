149bba4ea082a21ad26ef620f7d223c567d7954c
ShipItParrot <shipitparrot@gmail.com>
2024-08-25 12:01:43 +0800

    Add bonus challenge

---
+++ 20240825_arrays_and_bools/001a_check_length_of_list_again.py
@@ -0,0 +1,13 @@
+"""
+Create a list with 3 numbers
+
+my_list: list[int] = [1,3,5]
+
+get the length of the list and print it
+
+hint: use len(my_list) to get back the list length as an int
+
+my_list_length: int = len(my_list)
+
+print(my_list_length)
+"""
