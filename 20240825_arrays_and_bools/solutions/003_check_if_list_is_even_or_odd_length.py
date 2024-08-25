05c1eb5efd575daa39260d1a0f27f89730856854
ShipItParrot <shipitparrot@gmail.com>
2024-08-25 11:06:40 +0800

    Add third challenge

---
+++ 20240825_arrays_and_bools/solutions/003_check_if_list_is_even_or_odd_length.py
@@ -0,0 +1,16 @@
+"""
+Create a list with 7 numbers
+
+my_list: list[int] = [1,2,3,4,5,6,7]
+
+If the length of the list is even length, print "List is even length!"
+
+If the length of the list is odd length, print "List is odd length!"
+"""
+
+my_list: list[int] = [1,2,3,4,5,6,7]
+
+if len(my_list) % 2 == 0:
+    print("List is even length!")
+else:
+    print("List is odd length!")
