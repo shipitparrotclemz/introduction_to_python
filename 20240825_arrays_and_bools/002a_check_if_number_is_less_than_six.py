241dd8576157f006810bc0f6756f7d2b8933be40
ShipItParrot <shipitparrot@gmail.com>
2024-08-25 11:42:35 +0800

    Add second bonus

---
+++ 20240825_arrays_and_bools/002a_check_if_number_is_less_than_six.py
@@ -0,0 +1,20 @@
+"""
+define an int
+
+my_int: int = 10
+
+if my_int is less than 5:
+
+print it's less than 5
+
+else
+
+print it's more or equal to 5
+"""
+
+my_int: int = 10
+
+if my_int < 5:
+    print("it's less than 5")
+else:
+    print("it's more or equal to 5")
---
+++ 20240825_arrays_and_bools/solutions/002a_check_if_number_is_less_than_six.py
@@ -0,0 +1,13 @@
+"""
+define an int
+
+my_int: int = 10
+
+if my_int is less than 5:
+
+print it's less than 5
+
+else
+
+print it's more or equal to 5
+"""
