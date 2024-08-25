ec3997fd39e492837891277db99f9f9d103d4100
ShipItParrot <shipitparrot@gmail.com>
2024-08-25 11:04:45 +0800

    Add second challenge answer key

---
+++ 20240825_arrays_and_bools/solutions/002_check_if_number_is_even_or_odd.py
@@ -0,0 +1,16 @@
+"""
+Create an int 10
+
+my_int: int = 10
+
+if my_int is even, print "It's even!"
+
+if my_int is odd, print "It's odd"!
+"""
+
+my_int: int = 10
+
+if my_int % 2 == 0:
+    print("It's even!")
+else:
+    print("It's odd!")
