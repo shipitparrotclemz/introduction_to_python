38fa2ce969d4c571dfeec573c1f337d69641d51c
ShipItParrot <shipitparrot@gmail.com>
2024-08-25 10:58:27 +0800

    Add solution for first practice

---
+++ 20240825_arrays_and_bools/solutions/001_check_length_of_list.py
@@ -0,0 +1,10 @@
+"""
+Create a list
+
+my_list: list[int] = [1,2,3,4,5,6]
+
+Print out the length of this list
+"""
+my_list: list[int] = [1,2,3,4,5,6]
+length_of_list: int = len(my_list)
+print(length_of_list)
