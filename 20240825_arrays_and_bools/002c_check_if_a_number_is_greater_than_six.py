6550cf3fe7c52ae9e48c97f6911193acf7a76a16
ShipItParrot <shipitparrot@gmail.com>
2024-08-25 11:54:48 +0800

    Add second challenge

---
+++ 20240825_arrays_and_bools/002c_check_if_a_number_is_greater_than_six.py
@@ -0,0 +1,11 @@
+"""
+Create an int and assign it to 6
+
+if the number is greater than 6
+
+print "It's greater than 6!"
+
+else
+
+do nothing
+"""
---
+++ 20240825_arrays_and_bools/solutions/002c_check_if_a_number_is_greater_than_six.py
@@ -0,0 +1,15 @@
+"""
+Create an int and assign it to 6
+
+if the number is greater than 6
+
+print "It's greater than 6!"
+
+else
+
+do nothing
+"""
+
+my_int: int = 6
+if my_int > 6:
+    print("It's greater than 6!")
