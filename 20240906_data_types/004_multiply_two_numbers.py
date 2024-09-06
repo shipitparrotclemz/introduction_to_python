82973c16830b66c9f2d3f4e679019d865b2b7399
ShipItParrot <shipitparrot@gmail.com>
2024-09-06 17:08:35 +0800

    Add multiply

---
+++ 20240906_data_types/004_multiply_two_numbers.py
@@ -0,0 +1,15 @@
+"""
+Create two integer objects
+
+first integer object is 10
+
+second integer object is 20
+
+create third integer object as product of first number and second number
+
+print the third integer object
+"""
+my_first_int: int = 10
+my_second_int: int = 20
+my_third_int: int = my_first_int * my_second_int
+print(my_third_int)
