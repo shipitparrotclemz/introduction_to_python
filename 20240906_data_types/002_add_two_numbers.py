aa250fd77f4af5c763f1204e7adf9ad49122b484
ShipItParrot <shipitparrot@gmail.com>
2024-09-06 16:51:08 +0800

    Add second practice

---
+++ 20240906_data_types/002_add_two_numbers.py
@@ -0,0 +1,17 @@
+"""
+Define two int objects
+
+my_int_one
+give type hint of int
+assign value to 10
+
+my_int_two
+give type hint of int
+assign value to 20
+
+create a new variable my_int_three
+give type hint of int
+assign this to `+` of my_int_one and my_int_two
+
+print my_int_three
+"""
