8913b542dcbf28e9c3c930000c5818c1b79c6948
ShipItParrot <shipitparrot@gmail.com>
2024-09-06 17:14:57 +0800

    Add floor division

---
+++ 20240906_data_types/006_floor_division_two_numbers.py
@@ -0,0 +1,10 @@
+"""
+Create first int as 1
+Create second int as 2
+Create third int as first int floor division second int
+print third int
+"""
+my_first_int: int = 19
+my_second_int: int = 10
+my_third_int: int = my_first_int // my_second_int
+print(my_third_int)
