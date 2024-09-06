f251017408273243dee9e5b515b0949c794c9775
ShipItParrot <shipitparrot@gmail.com>
2024-09-06 17:11:54 +0800

    Add division

---
+++ 20240906_data_types/005_divide_two_numbers.py
@@ -0,0 +1,16 @@
+"""
+create first int assign it to 10
+create second int assign it to 20
+create third float assign it to first int divide by second int
+
+float -> number with decimal points, 1.0 0.5 10.0
+int -> whole numbers like 1, 2, 3
+
+print third float
+"""
+
+my_first_int: int = 20
+my_second_int: int = 20
+my_third_float = my_first_int / my_second_int
+print(my_third_float)
+print(type(my_third_float))
