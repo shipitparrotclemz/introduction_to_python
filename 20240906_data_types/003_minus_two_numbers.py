1c624e7afc66e87487ff9bcf1797bb3e2afd9630
ShipItParrot <shipitparrot@gmail.com>
2024-09-06 17:05:50 +0800

    Minus two numbers with -

---
+++ 20240906_data_types/003_minus_two_numbers.py
@@ -0,0 +1,18 @@
+"""
+Create first int variable, call it my_first_int
+give type hint of int
+assign it's value to 10
+
+Create second int variable, call it my_second_int
+give type hint of int
+assign it's value to 20
+
+Create third int variable, assign it to my_first_int subtract by my_second_int
+give type hint of int
+
+print my_third_int
+"""
+my_first_one: int = 10
+my_second_one: int = 20
+my_three_one: int = my_first_one - my_second_one
+print(my_three_one)
