0d92d07c89cc59d9765bf9aac8b3fbae388700e2
ShipItParrot <shipitparrot@gmail.com>
2024-10-06 14:34:11 +0800

    Add two new concepts

--- additional_practice_20231210/007_lessons_on_dictionaries/bonus_questions_20241006/001_creating_a_skill_tree.py
+++ additional_practice_20231210/007_lessons_on_dictionaries/bonus_questions_20241006/001_creating_a_skill_tree.py
@@ -22,6 +22,7 @@ Create a skill tree for jobs
     }
 }
 """
+import time
 from typing import Any
 
 skill_tree: dict[str, dict[str, dict[str, dict[str, Any]]]] = {
@@ -44,3 +45,10 @@ skill_tree: dict[str, dict[str, dict[str, dict[str, Any]]]] = {
         }
     }
 }
+
+# New concepts:
+# 1. while <bool>
+# 2. time.sleep(1) -> allows you to wait for 1 second before running
+while True:
+    time.sleep(1)  # wait for 1 second befor running
+    print("Please enter your input")
