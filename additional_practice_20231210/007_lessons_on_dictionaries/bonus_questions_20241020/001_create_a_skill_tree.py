07259dc66e88ec427be409b5d7c81f2fc72fb4ef
ShipItParrot <shipitparrot@gmail.com>
2024-10-20 14:27:33 +0800

    Add skill tree

---
+++ additional_practice_20231210/007_lessons_on_dictionaries/bonus_questions_20241020/001_create_a_skill_tree.py
@@ -0,0 +1,21 @@
+"""
+Create a skill tree dictionary
+
+Part 1:
+"""
+from typing import Any
+skill_tree: dict[str, Any] = {
+    "warrior": {
+        "skills": {}
+    },
+    "magician": {
+        "skills": {}
+    },
+    "archer": {
+        "skills": {}
+    },
+    "thief": {
+        "skills": {}
+    },
+}
+
---
+++ additional_practice_20231210/007_lessons_on_dictionaries/bonus_questions_20241020/README.md
@@ -0,0 +1,10 @@
+# Dictionary Bonus Revisions
+
+## Objective:
+- Further improve concepts on dict
+- Creating a dict
+- Inserting new key to dict
+- Checking if key exists in a dict
+- Popping / Deleting a key from dict
+- Update a key in dict
+
