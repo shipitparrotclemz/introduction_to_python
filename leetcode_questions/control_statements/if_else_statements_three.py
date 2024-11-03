52a43abb460874181f478f69c277dcdd177330a8
ShipItParrot <shipitparrot@gmail.com>
2024-11-03 15:12:51 +0800

    Add if else statements

---
+++ leetcode_questions/control_statements/if_else_statements_three.py
@@ -0,0 +1,18 @@
+"""
+
+if sugar_intake is more than 100g + and bmi is more than or equal to 27.5
+
+print "Risk of Diabetes"
+
+if sugar_intake is more than 100g + and bmi is less than 27.5
+
+print "Low Risk of Diabetes"
+
+if sugar intake is less than 100g + bmi is more than or equal to 27.5
+
+print "Low Risk of Diabetes"
+
+if sugar intake is less than 100g + bmi is less than 27.5
+
+print "No risk of Diabetes"
+"""
