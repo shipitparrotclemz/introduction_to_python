8da7065e3c30603f700da397adf15ea460985e03
ShipItParrot <shipitparrot@gmail.com>
2024-11-03 14:45:27 +0800

    Add question 2

---
+++ leetcode_questions/control_statements/if_else_statements_two.py
@@ -0,0 +1,15 @@
+"""
+Given a grade 1 to 100
+
+If grade 90 - 100, set answer = A
+
+If grade 80 - 89, set answer = B
+
+If grade 70 - 79, set answer = C
+
+If grade 60 - 69, set answer = D
+
+If grade 50 to 59, set answer = E
+
+Else grade 1 to 49, set answer = F
+"""
