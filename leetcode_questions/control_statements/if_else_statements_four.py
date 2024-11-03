8630056dc5fa65be7a7d4b37e6f4c16baeaa8803
ShipItParrot <shipitparrot@gmail.com>
2024-11-03 15:39:36 +0800

    Add if else statement four

---
+++ leetcode_questions/control_statements/if_else_statements_four.py
@@ -0,0 +1,24 @@
+"""
+
+Given name
+
+If name is "Xuan"
+
+set drink to "Matcha Latte"
+
+If name is "Aaron"
+
+set drink to "Caramel Macchiato"
+
+If name is "Gabriel"
+
+set drink to "Latte"
+
+If name is "Clement"
+
+set drink to "Cold Brew"
+
+If name is "Joyce"
+
+set drink to "Teh Peng"
+"""
