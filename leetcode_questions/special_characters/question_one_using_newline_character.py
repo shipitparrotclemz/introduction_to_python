ecf55606f14b9001d616bb1da55290eeabfd7f83
ShipItParrot <shipitparrot@gmail.com>
2024-11-10 13:58:55 +0800

    Add question on new line character

---
+++ leetcode_questions/special_characters/question_one_using_newline_character.py
@@ -0,0 +1,13 @@
+"""
+
+Create a string variable that prints the following
+
+ranges have three parameters
+start is the starting index
+end is the ending index, exclusive
+step is the steps to take at every iteration
+so range(1, 10, 2) will give
+1,3,5,7,9
+
+Then print it
+"""
---
+++ leetcode_questions/special_characters/string_newline_character.py
@@ -0,0 +1,11 @@
+"""
+Q: What is "\n"
+"""
+
+my_string: str = "Gabriel did his homework\nHis if else statements are now strong"
+
+# Expected:
+# Gabriel did his homework
+# His if else statements are now strong
+
+print(my_string)
