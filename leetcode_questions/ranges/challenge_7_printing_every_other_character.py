e2dfd19da28790f0a976d741d9f8afc7819f8e0f
ShipItParrot <shipitparrot@gmail.com>
2024-11-10 14:47:07 +0800

    Add challenges

---
+++ leetcode_questions/ranges/challenge_7_printing_every_other_character.py
@@ -0,0 +1,11 @@
+"""
+Print only every 2 character
+
+A
+r
+n
+
+Use range + char at word index
+"""
+word = "Aaron"
+
---
+++ leetcode_questions/ranges/challenge_8_printing_every_character_but_backwards.py
@@ -0,0 +1,20 @@
+name = "Gabriel"
+
+"""
+Print
+
+l
+e
+i
+r
+b
+a
+G
+
+Hint 1: Use range(start, end, step), but step is -1
+Hint 2: Get a character from a word by index
+
+word = "Gabriel"
+# I want i
+print(word[4])
+"""
