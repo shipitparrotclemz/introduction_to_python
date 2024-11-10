4cfa165622c54e6a95437bc3da55b502881b8fd1
ShipItParrot <shipitparrot@gmail.com>
2024-11-10 14:14:51 +0800

    Add challenge one

---
+++ leetcode_questions/ranges/challenge_one_iterating_forward.py
@@ -0,0 +1,37 @@
+"""
+Using a for loop and a range function
+
+print
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+
+range(start, end, step)
+
+if you don't provide start and step
+
+e.g
+
+range(6)
+
+6 will be inferred as end
+So it will print
+1
+2
+3
+4
+5
+
+If you do range(start, end)
+
+range(1, 6)
+
+step will be inferred as 1 by default
+"""
