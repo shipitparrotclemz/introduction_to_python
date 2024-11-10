574d3efa65da83d0940e7eebcbe138846519df01
ShipItParrot <shipitparrot@gmail.com>
2024-11-10 15:26:32 +0800

    Add challenges

---
+++ leetcode_questions/ranges/challenge_10_range_7_to_3_but_print_char.py
@@ -0,0 +1,12 @@
+"""
+Given Charizard
+
+Print characters from index 7 to 3
+
+r
+a
+z
+i
+r
+
+"""
---
+++ leetcode_questions/ranges/challenge_11_fly_me_to_the_moon.py
@@ -0,0 +1,13 @@
+sentence = "Fly me to the moon"
+
+"""
+Use string slicing; bonus questions if you can use for-loops too
+
+Answer: ["Fly", "me", "to", "the", "moon"]
+
+Hint: To append to a list
+- my_list.append("Fly")
+
+Hint: To slice Fly
+- sentence[0:3]
+"""
