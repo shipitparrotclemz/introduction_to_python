cc7b7d57e197a2637c4a8ce4bfbf13792ff993c8
ShipItParrot <shipitparrot@gmail.com>
2024-11-10 13:51:45 +0800

    Add if else five

---
+++ leetcode_questions/control_statements/if_else_statements_five.py
@@ -0,0 +1,19 @@
+"""
+Q:
+
+You are given a sugar level variable containing a float from 0g / 100ml to 10g/100ml
+
+If
+
+1. sugar level from 0 <= sugar_level <= 2.5
+- Give it A
+
+2. sugar level from 2.5 < sugar_level <= 5.0
+- Give it B
+
+3. sugar level from 5.0 < sugar_level <= 7.5
+- Give it C
+
+4. sugar_level from 7.5 < sugar_level
+- Give it D
+"""
