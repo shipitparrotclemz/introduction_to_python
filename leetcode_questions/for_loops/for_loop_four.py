91a79ec82339d0e6a64e7e4eb5cd7c97c2236e83
ShipItParrot <shipitparrot@gmail.com>
2024-11-17 15:32:32 +0800

    Add loop four

---
+++ leetcode_questions/for_loops/for_loop_four.py
@@ -0,0 +1,9 @@
+"""
+Given a list of numbers from 1 to 100
+
+Create another list of prime numbers
+
+A prime number is a natural number greater than 1
+
+that has no positive divisors other than 1 and itself.
+"""
