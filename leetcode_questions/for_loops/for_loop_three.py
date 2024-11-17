2f30fc9b49ebe9633558c4110af24fb5c9ebb116
ShipItParrot <shipitparrot@gmail.com>
2024-11-17 14:33:45 +0800

    Add loop three

---
+++ leetcode_questions/for_loops/for_loop_three.py
@@ -0,0 +1,12 @@
+"""
+Challenge 3:
+
+Print all odd numbers from 0 to 100
+
+1,3,5,7...
+
+Prerequisites:
+- For-Loops
+- (Optional) % to get the remainer of a number
+- (Optional) step in range
+"""
