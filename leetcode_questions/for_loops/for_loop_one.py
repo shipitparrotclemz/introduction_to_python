237de8cb9266f4571e8dbc2fb431bac8af299bd2
ShipItParrot <shipitparrot@gmail.com>
2024-11-17 13:54:25 +0800

    Add for loop challenge

---
+++ leetcode_questions/for_loops/for_loop_one.py
@@ -0,0 +1,14 @@
+"""
+Challenge 1:
+
+Given a list of names
+
+Print each name on it's own line
+
+Prerequisites:
+- For loops
+- Print
+- Iterating a List
+"""
+
+names: list[str] = ["Aaron", "Gabriel", "Elson", "Wei Xuan", "Zheng Yang", "Eugene"]
